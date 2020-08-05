d261ae3385152fe7b1db8911d04e68167957cefb
daniela ramirez <daniedi.solution@gmail.com>
2020-08-05 09:10:40 -0500

    register

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+//import { myFunction } from './lib/index.js';
 
-myFunction();
+//myFunction()
+ 
+     
+const register = () => {
+
+    let Name = document.getElementById("nombre");
+    let User = document.getElementById("nickName");
+    let email = document.getElementById("emailRegister").value;
+    let password = document.getElementById("passRegister").value;
+    console.log(email);
+    console.log(password);
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+     // Handle Errors here.
+     let errorCode = error.code; 
+     let errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      //...
+    });
+    }
+    let btnRegister=document.getElementById("btnRegister");
+    btnRegister.addEventListener("click", register);
+     
