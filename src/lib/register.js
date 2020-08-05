b893e1d8c46647af233df7da0d578972c9da9c61
Caro <cslondo90@gmail.com>
2020-08-05 18:18:22 -0500

    arreglada función userRegister()

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,20 +1,22 @@
-const register = () => {
-    var email = document.getElementById("emailRegister").value;
-    var password = document.getElementById("passRegister").value;
-    console.log(email);
-    console.log(password);
+const register = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    alert("La contrseña debe tener al menos 6 caracteres");
+    // ...
+  });
+}
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-        alert("La contrseña debe tener al menos 6 caracteres");
-        // ...
-      });
-
+const userRegister = () => {
+  var userEmail = document.getElementById("emailRegister").value;
+  var userPassword = document.getElementById("passRegister").value;
+  register(userEmail, userPassword)
+  console.log(userEmail);
+  console.log(userPassword);
 }
 
 var btnRegister=document.getElementById("btnRegister");
-btnRegister.addEventListener("click", register);
+btnRegister.addEventListener("click", userRegister);
