c7c548c4a9c1f2817df9d594c14f890dba62d194
daniela ramirez <daniedi.solution@gmail.com>
2020-08-05 13:54:56 -0500

    register

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,20 +1,30 @@
-const register = () => {
-    var email = document.getElementById("emailRegister").value;
-    var password = document.getElementById("passRegister").value;
-    console.log(email);
-    console.log(password);
+import  funciones  from './lib/firebaseConfig.js';
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-        alert("La contrseña debe tener al menos 6 caracteres");
-        // ...
-      });
+var email = document.getElementById("emailRegister").value;
+var password = document.getElementById("passRegister").value;
+var btnRegister=document.getElementById("btnRegister");
+   
+const register = (event) => {
+email = event.target.email.value;
+ password = event.target.password.value;
+  
+firebase.auth().createUserWithEmailAndPassword(email, password).then(result =>{
+ 
+alert("verifica tu correo para terminar el registro");
 
+}).catch(error =>{
+// Handle Errors here.
+var errorCode = error.code;
+var errorMessage = error.message;
+console.log(errorCode);
+console.log(errorMessage);
+alert("La contrseña debe tener al menos 6 caracteres");
+
+})
+         
+      
 }
 
-var btnRegister=document.getElementById("btnRegister");
-btnRegister.addEventListener("click", register);
+
+ 
+btnRegister.addEventListener("submit", register);
