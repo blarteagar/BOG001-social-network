49e744edcd717203de9f0af961f1208ddf282166
blarteagar <64844621+blarteagar@users.noreply.github.com>
2020-08-14 16:54:54 -0500

    iniciando wall

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
-import wall from './wall/wall.js'
-import {registerView} from "./register/register.js";
+import { wallView } from "./wall/wall.js";
+import { registerView } from "./register/register.js";
 import login from "./Login/login.js";
 
 let btnPrueba = document.getElementById("btnPrueba");
@@ -8,29 +8,28 @@ btnPrueba.addEventListener("click", () => { router("#/register") });
 let btnWall = document.getElementById("btnWall");
 btnWall.addEventListener("click", () => { router("#/wall") });
 
-export const router =  async (route) => {
-    
+export const router = async (route) => {
+
     const content = document.getElementById("root");
-    
+
     switch (route) {
-        case "#/register": {
-            window.location.hash="#/register";
-            content.innerHTML= registerView.render();
-            await registerView.afterRender();
+        case "#/login": {
+            content.innerHTML = login();
             break;
-        } 
-        case "#/login":{
-            content.innerHTML =  login();
+        }
+        case "#/register": {
+            window.location.hash = "#/register";
+            content.innerHTML = registerView.render();
+            await registerView.afterRender();
             break;
         }
         case "#/wall": {
-            console.log(wall);
-            console.log(typeof(wall));
-            window.location.hash = "#/wall"
-            return content.appendChild(wall());
+            window.location.hash = "#/wall";
+            content.innerHTML = wallView.render();
+            await wallView.afterRenderWall();
             break;
         }
         default:
-                     content.innerHTML =  login();
+            content.innerHTML = login();
     }
 }
--- src/wall/wall.js
+++ src/wall/wall.js
@@ -1,24 +1,32 @@
+import { editPost } from "/wall/editPost.js";
+import { erasePost } from "/wall/erasePost.js";
 
-const wall = () => {
+export const wallView = {
+
+  renderWall: () => {
     let content = document.getElementById("root");
     content.innerHTML = "";
-    let viewWall =
+    const viewWall =`
+        
+        <section id = "mainWall">
+        <p> Welcome to your Wall </p>
+        <p> Here you can see your posts, edit them or erase them </p>
+        <p> also you can see how many likes they have </p>
+        <p> Enjoy! </p>
+        <button id="btnEdit"> Ed </button>
+        <button id="btnErase"> Er </button>
+        </section>
+        
     `
-    
-    <section id = "mainWall">
-    <p> Welcome to your Wall </p>
-    <p> Here you can see your posts, edit them or erase them </p>
-    <p> also you can see how many likes they have </p>
-    <p> Enjoy! </p>
-    <button id="btnEdit"> Ed </button>
-    </section>
-    
-    `
-    ;
+    return viewWall
+  },
 
-    let divElement = document.createElement('div');
-    divElement.innerHTML = viewWall;
-    return divElement;
-};
-    
-export default wall;
+  afterRenderWall: async () => {
+    let btnEdit = document.getElementById("btnEdit");
+    btnEdit.addEventListener("click", editPost);
+
+    let btnErase = document.getElementById("btnErase");
+    btnErase.addEventListener("click", alert ('edit'));
+  },
+
+}
