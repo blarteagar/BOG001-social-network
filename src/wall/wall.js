13eb5e6e6b485f35fcb67d7cbfd0b8bffc80cfe0
blarteagar <64844621+blarteagar@users.noreply.github.com>
2020-08-24 09:57:41 -0500

    wall grid

--- src/wall/wall.js
+++ src/wall/wall.js
@@ -9,35 +9,40 @@ const wallView = {
     const view = `
     
     <div class="grid">
-    <div class="logoWall">
-    <h1 class="techWall"> |Tech<span class="meWall">Me</span></h1></div>
-    </div>
-    <div class="navWall">
-    <span class="itemWall"><button id="btnWallWall">muro</button></span>
-    <span class="itemWall"><button id="btnNewsWall">noticias</button></span>
-    <span class="itemWall"><button id="btnOutWall">salir</button></span>
-    </div>
-    <div class="hereWall">
-    <p>Aquí podrás compartir tu idea, apoyar otros proyectos, dar like y compartir tus conocimientos</p>
-    </div>
-    <div class="ideaWall">
-    <p>Comparte la idea de tu proyecto</p>
-    </div>
-    <div class="shareWall">
-    <p class="shareText">Escribe en este espacio:</p>
-    <input id = "postWall" class = "writePost" type = textarea maxlength = 120 placeholder = "Al contrario de la creencia popular, el texto de Lorem Ipsum no">
-    <button id="btnShareWall">compartir</button>
-    <button id="btnEditWall">editar</button>
-    <button id="btnEraseWall">borrar</button>
-    </div>   
-    </div>
-    <div class="footWall">
-    <p>Made with love by Laboratorians</p>
-    <p>(c) Blanca Arteaga - Caro Suarez - Daniela Ramírez 2020</p>
-    </div>    
-    </div>
+      <div class="logoWall">
+        <h1 class="techWall"> |Tech<span class="meWall">Me</span></h1></div>
+      </div>
 
-    `
+      <div class="navWall">
+        <span class="itemWall"><button id="btnWallWall">muro</button></span>
+        <span class="itemWall"><button id="btnNewsWall">noticias</button></span>
+        <span class="itemWall"><button id="btnOutWall">salir</button></span>
+      </div>
+
+      <div class="hereWall">
+        <p>Aquí podrás compartir tu idea, apoyar otros proyectos, dar like y compartir tus conocimientos</p>
+      </div>
+
+      <div class="ideaWall">
+        <p>Comparte la idea de tu proyecto</p>
+      </div>
+
+      <div class="shareWall">
+        <p class="shareText">Escribe en este espacio:</p>
+        <input id = "postWall" class = "writePost" type = textarea maxlength = 120 placeholder = "Al contrario de la creencia popular, el texto de Lorem Ipsum no">
+        <button id="btnShareWall">compartir</button>
+        <button id="btnEditWall">editar</button>
+        <button id="btnEraseWall">borrar</button>
+      </div>   
+
+      <div class="footWall">
+        <p>Made with love by Laboratorians</p>
+        <p>(c) Blanca Arteaga - Caro Suarez - Daniela Ramírez 2020</p>
+      </div>
+
+  </div>
+
+`
 
     return view;
   },
