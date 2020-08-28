5c42fddbe15fc1b3df300aec42ac1a18180dd942
Caro <cslondo90@gmail.com>
2020-08-28 14:06:26 -0500

    botón de like ya funcionando falta contador

---
+++ src/FireFunctions/likes.js
@@ -0,0 +1,73 @@
+export const likeFunction = async (postId) => {
+  let db = await firebase.firestore();
+  let auth = await firebase.auth().currentUser;
+  let docRef = await db
+    .collection("Posts")
+    .doc(postId)
+    .collection("likes" + postId)
+    .doc("likeFrom" + auth.uid);
+
+  docRef.get().then((doc) => {
+    if (!doc.exists) {
+      docRef.set({
+          userId: auth.uid,
+          postId: postId,
+          like: 1,
+        })
+        .then(() => {
+          console.log(
+            "el usuario" + auth.uid + "ha dado like en el post" + postId
+          );
+          console.log("se ha creado el documento de like:" + docRef.id);
+          console.log("este documento tiene una data:");
+          
+        })
+        .catch((error) => {
+          console.error("ha habido algún error dando like:" + error);
+        });
+    } else if (doc.data().like == 1) {
+      return docRef
+        .update({
+          like: 0,
+        })
+        .then(function () {
+          console.log("el like estaba en true y pasó a false");
+        })
+        .catch(function (error) {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        });
+    } else if (doc.data().like == 0) {
+      return docRef
+        .update({
+          like: 1,
+        })
+        .then(function () {
+          console.log("el like estaba en false y pasó a true");
+        })
+        .catch(function (error) {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        });
+    }
+  });
+};
+
+export const counterFunction = async (postId) => {
+  
+  let db = await firebase.firestore();
+  let auth = firebase.auth().currentUser;
+  let docRefPost = await db.collection("Posts").doc(postId).collection("likes" + postId);
+
+  try {
+
+    let likesCollection = await docRefPost.where("like", "==", 1).get();
+    return likesCollection.docs.length
+
+
+
+  } catch (error) {
+      console.log(error)
+  }
+
+};
